3723c6d8fe4c79a79f23fdf7eee4446128658962
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-26 16:10:00 +0100

    aggiunta commenti

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 //mostrare / nascondere il menu principale usando jq
 
 
-//quando clicco il menu compare
-
+//quando clicco il menu compare uso classe .fa-bars(html) e hamburger-menu.active(css)
+//usando funz al click su fa-bars richiamando hamburger menu active dalla media query
 $( ".fa-bars" ).click(function() {
 
    $( ".hamburger-menu" ).addClass( "active" );
  });
 
-//quando clicco sulla x il menu scompare
-
+//quando clicco sulla x il menu scompare uso classe .fa-times(html) e hamburger-menu.active(css)
+////usando funz al click su fa-times richiamando hamburger menu active dalla media query
 $( ".fa-times" ).click(function() {
 
    $( ".hamburger-menu" ).removeClass( "active" );
